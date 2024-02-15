daeddb5bfca25e39b97b3db00e52c7d6ddb58c31
Sebastian Nozzi <sebnozzi@gmail.com>
2024-02-15 22:27:25 +0100

    run specified script in attribute

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ jQuery(($: any, _: any) => {
   const msTerm = new MSTerminal();
   msTerm.setup();
 
-  const mainFile = "demo.ms";
+  const body = document.querySelector("body") as HTMLBodyElement;
+  const mainFile = body.getAttribute("data-src-file");
+  if (typeof mainFile !== "string") {
+    throw new Error("No source file specified!");
+  }
 
   fetch(mainFile).then((response) => {
     return response.text();
